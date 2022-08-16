c5a128d120cd4fb9a3e5a4fb43dbf71eec2a89f1
Ryan <69286341+MegaMidnight@users.noreply.github.com>
2022-08-16 16:24:04 -0700

    Mastermind Game added.

---
+++ src/main/kotlin/EvaluateGuess.kt
@@ -0,0 +1,22 @@
+package mastermind
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
+
+    val commonLetters = "ABCDEF".sumOf { ch ->
+
+        secret.count { it == ch }.coerceAtMost(guess.count { it == ch })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
+}
+
+fun main() {
+    evaluateGuess("BCDF", "ACEB")
+    evaluateGuess("AAAF", "ABCA")
+    evaluateGuess("ABCA", "AAAF")
+}
+
+
